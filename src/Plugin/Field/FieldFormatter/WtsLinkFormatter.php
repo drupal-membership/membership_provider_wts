b396a1db0c2af3affdd3d4e514d6f122444bf1fe
Brad Jones <brad@jones.name>
2016-12-28 11:21:16 -0700

    Add field configs

---
+++ src/Plugin/Field/FieldFormatter/WtsLinkFormatter.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Drupal\membership_provider_wts\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'wts_link' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "wts_link",
+ *   label = @Translation("WTS Signup Link"),
+ *   field_types = {
+ *     "wts_memtype"
+ *   }
+ * )
+ */
+class WtsLinkFormatter extends FormatterBase {
+
+  /**
+   * The signup URL.
+   *
+   * @var string
+   */
+  const SIGNUP_URL = 'https://join.achbill.com/Signup/signup.cgi';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      // Implement default settings.
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
